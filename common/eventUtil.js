6dd2d50e2817bc02b676a462a6b2c0b6c9f2d9b7
RaynerBan <ban2012liaopr@gmail.com>
2014-09-15 21:32:26 +0800

    eventUtil

--- common/eventUtil.js
+++ common/eventUtil.js
@@ -17,9 +17,74 @@ var EventUtil = {
 			element['on'+type] = null;
 		}
 	},
+	getEvent : function(e){
+		e = window.event || e;
+		return e;
+	},
 	getEventTarget : function(e){
 		e = window.event || e;
+		//e = this.getEvent();
 		return e.srcElement || e.target;
+	},
+	// 得到相关对象针对mouserover ,mouserout
+	//一个主要对象，一个相关对象
+	getRelatedTarget : function(e){
+		if(e.relatedTarget){
+			return e.relatedTarget;
+		}else if(e.toElement){
+			return e.toElement;
+		}else if(e.fromElement){
+			return e.fromElement;
+		}
+	},
+	stopPropagation : function(e){
+		if(e.stopPropagation){
+			e.stopPropagation;
+		}else{
+			event.cancelBubble = true; //case IE
+		}
+	},
+	getCharKode : function(e){
+		if(e.charCode){
+			return e.charCode;
+		}else if(e.keyCode){
+			return e.keyCode;
+		}
+	},
+	each : function(array,callback){
+		if (Object.prototype.toString.call(array) !== '[object Array]'){
+			return;
+		}
+		for(var i= 0,len = array.length;i<len;i++){
+			callback.call(array[i],i,array[i]);
+		}
+	},
+	trim : function(str){
+		if(typeof String.prototype.trim !== 'function') {
+				return str.replace(/^\s+|\s+$/g, '');
+		}else{
+			return str.trim();
+		}
+	},
+	/* config = { 	method : "get/post",
+	 *				url : "",
+	 *				success : function}
+	*/
+	ajax : function(config){
+		var xhr = new window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsfr.XMLHTTP');
+		xhr.onreadystatechange = function(){
+             if(xhr.readyState == 4 && xhr.status == 200){
+                 config.success && config.success(xhr);
+             }
+         };
+		if(config.method){
+             xhr.open(config.method,config.url,true);
+             config.method == 'get' ?
+                 xhr.send() :
+                 (xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded"),
+                     xhr.send(serialize(config.data)));
+
+         }
 	}
 };
 
@@ -64,7 +129,7 @@ $('input.cascade').on('keyup',function(){
 
 if(typeof String.prototype.trim !== 'function') {
   String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/g, ''); 
+    return this.replace(/^\s+|\s+$/g, '');
   }
 }
 
